2be352e32595eb1f33841c6adcd83d8a11e14c8d
cristian <cristianprodius1@gmail.com>
2024-07-18 20:04:50 +0300

    home page almost done need the lionks to make them work

--- src/assets/components/SingleQuestion.tsx
+++ src/assets/components/SingleQuestion.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { BiMinus } from "react-icons/bi";
 import { BsPlusLg } from "react-icons/bs";
 
--- src/assets/pages/HomePage.tsx
+++ src/assets/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import BeforeAfter from "../components/BeforeAfter";
 import FAQ from "../components/FAQ";
 import Features from "../components/Features";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Programs from "../components/Programs";
@@ -14,6 +15,18 @@ const HomePage = () => {
       <Programs />
       <BeforeAfter />
       <FAQ />
+      <div className="bg-main max-w-[1240px] py-8 text-center mx-auto flex flex-col items-center justify-center rounded-[30px] mb-12">
+        <h1 className="mb-8 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl uppercase ">
+          Are you ready to change your life?
+        </h1>
+        <a
+          href="#"
+          className="text-white bg-[#2b2b2b] hover:text-main hover:bg-secondary font-bold rounded-lg text-xl px-12 py-5 text-center"
+        >
+          Join Now
+        </a>
+      </div>
+      <Footer />
     </div>
   );
 };
